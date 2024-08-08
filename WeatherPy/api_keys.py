1a0e8bf511ca76f9a3dae4dfc4fb1516d44528bc
Andrew_Sobiech <andrewsobiech@Andrews-MBP.attlocal.net>
2024-08-08 11:36:07 -0400

    File changes

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,3 +1,5 @@
-# API Key Open Weather
+# OpenWeatherMap API Key
+weather_api_key = "d694eeaea6a1db71be0f25e074505055"
 
-api_key = "d694eeaea6a1db71be0f25e074505055"
+# Geoapify API Key
+geoapify_key = "33ed392172dc4ce5a7fcd490c8f0cb13"
